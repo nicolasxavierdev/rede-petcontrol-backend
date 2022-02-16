8ee86fe939af000773de992e6a2f27380cfa65b2
Nícolas Alexandre <nicolas.xavier@cobasi.com.br>
2022-02-16 17:32:52 -0300

    criado schema consultasModel

---
+++ models/consultasModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  consultationDate: {
+    type: String,
+    required: true
+  },
+  client: {
+    type: String,
+    required: true
+  }
+});
+
+const consultasModel = mongoose.model('Consultas', schema);
+
+module.exports = consultasModel;
