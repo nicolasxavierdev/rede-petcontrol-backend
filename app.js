daf068bb676277bfef1364419968c9cb5b517682
Nícolas Alexandre <nicolas.xavier@cobasi.com.br>
2022-02-16 16:52:25 -0300

    adicionado petsRoute

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const userRoute = require('./routes/clientesRoute');
+const clientesRoute = require('./routes/clientesRoute');
+const petsRoute = require('./routes/petsRoute');
+
 
 const app = express();
-app.use('/clientes', userRoute);
 
 app.use(bodyParser.json());
 mongoose.connect("mongodb://localhost:27017/redePetControl");
+
+app.use('/clientes', clientesRoute);
+app.use('/pets', petsRoute);
+
 
 app.listen(3000, () => {
   console.log('rodando na porta 3000');
