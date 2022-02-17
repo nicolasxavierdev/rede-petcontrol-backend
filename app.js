5f4d1183c4ed1766902ed52f351a4af3ffbe83d5
Nícolas Alexandre <nicolas.xavier@cobasi.com.br>
2022-02-17 14:31:37 -0300

    organizando

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const clientesRoute = require('./routes/clientesRoute');
-const petsRoute = require('./routes/petsRoute');
 const consultasRoute = require('./routes/consultasRoute');
+const petsRoute = require('./routes/petsRoute');
 
 const app = express();
 
@@ -11,8 +11,8 @@ app.use(bodyParser.json());
 mongoose.connect("mongodb://localhost:27017/redePetControl");
 
 app.use('/clientes', clientesRoute);
-app.use('/pets', petsRoute);
 app.use('/consultas', consultasRoute);
+app.use('/pets', petsRoute);
 
 app.listen(3000, () => {
   console.log('rodando na porta 3000');
